1d3588f175ef63126ad391e1535cc2ea9a641725
unikal1 <49333855+unikal1@users.noreply.github.com>
2024-06-16 15:13:56 +0900

    init commit

---
+++ src/main/java/com/onebucket/OneBucketApplication.java
@@ -0,0 +1,13 @@
+package com.onebucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneBucketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneBucketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onebucket/OneBucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onebucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneBucketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
