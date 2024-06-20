cb21e82d7410173f5ca41167978715388bf728be
unikal1 <49333855+unikal1@users.noreply.github.com>
2024-06-20 17:44:40 +0900

    [FEAT] jwt auth init commit
    Add JwtToken        : Save tokens and type
    Add JwtProvider     : generate token
    Add JwtProviderTest : use mockUser and anonymousUser

---
+++ src/main/java/com/onebucket/gloabal/auth/jwtAuth/component/JwtProvider.java
@@ -0,0 +1,148 @@
+package com.onebucket.gloabal.auth.jwtAuth.component;
+
+import com.onebucket.gloabal.auth.jwtAuth.domain.JwtToken;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+/**
+ * <br>package name   : com.onebucket.gloabal.auth.jwtAuth.component
+ * <br>file name      : JwtProvider
+ * <br>date           : 2024-06-20
+ * <pre>
+ * <span style="color: white;">[description]</span>
+ * This component is responsible for generating JWT tokens, including both access and refresh tokens.
+ * It utilizes the io.jsonwebtoken library to create tokens signed with a provided secret key.
+ * </pre>
+ * <pre>
+ * <span style="color: white;">usage:</span>
+ * {@code
+ * Authentication authentication = ...;
+ * JwtProvider jwtProvider = new JwtProvider("secretKey", 3600000, 7200000);
+ * JwtToken jwtToken = jwtProvider.generateToken(authentication);
+ * System.out.println("Access Token: " + jwtToken.getAccessToken());
+ * System.out.println("Refresh Token: " + jwtToken.getRefreshToken());
+ * } </pre>
+ * <pre>
+ * modified log :
+ * =======================================================
+ * DATE           AUTHOR               NOTE
+ * -------------------------------------------------------
+ * 2024-06-20        jack8              init create
+ * </pre>
+ */
+
+@Component
+public class JwtProvider {
+
+    private final Key key;
+    private final long expireDateAccessToken;
+    private final long expireDateRefreshToken;
+
+
+    /**
+     * Constructor of JwtProvider class.  Parameters of constructor are from {@code application.properties}.
+     * Make secret key to key byte with Base64 Decoder and Sha algorithm.
+     * @param secretKey from application.properties / to decode token
+     * @param expireDateAccessToken from application.properties
+     * @param expireDateRefreshToken from application.properties
+     */
+    @Autowired
+    public JwtProvider(@Value("${jwt.secret}") String secretKey,
+                       @Value("${jwt.expireDate.accessToken}") long expireDateAccessToken,
+                       @Value("${jwt.expireDate.refreshToken}") long expireDateRefreshToken) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+
+        this.expireDateAccessToken = expireDateAccessToken;
+        this.expireDateRefreshToken = expireDateRefreshToken;
+    }
+
+    /**
+     * <p>
+     *     public method of JwtProvide class to generate access token and refresh token wrapped in {@link JwtToken}
+     *     which type is {@code Bearer}. Get authentication from security context holder.
+     * </p>
+     * @param authentication given in {@link org.springframework.security.core.context.SecurityContextHolder SecurityContextHolder}
+     * @return JwtToken - contain token and type.
+     * @see JwtToken
+     */
+    public JwtToken generateToken(Authentication authentication) {
+
+        if(authentication == null
+                || authentication.getPrincipal() == null
+                || !(authentication.getPrincipal() instanceof User)) {
+            throw new IllegalArgumentException("Ivalid authentication object");
+        }
+        long nowDate = (new Date()).getTime();
+        String accessToken = generateAccessToken(authentication, nowDate);
+        String refreshToken = generateRefreshToken(nowDate);
+        //implement this
+        return JwtToken.builder()
+                .grantType("Bearer")
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+
+    /**
+     * <p>
+     *     Generate Access Token by given Authentication with now-date.
+     *     Sign with given key in {@code application.properties}, with HS256.
+     *     Token also contains expire date and subject which from user name in authentication.
+     * </p>
+     * @param authentication from generateToken
+     * @param date String with java Date format
+     * @return jwt string contain username, expire, authorities, sign key
+     */
+    private String generateAccessToken(Authentication authentication, long date) {
+        Date tokenExpiration = new Date(date + expireDateAccessToken);
+        String authorities = getAuthoritiesFromAuthentication(authentication);
+
+        return Jwts.builder()
+                .setSubject(authentication.getName())
+                .setExpiration(tokenExpiration)
+                .claim("auth", authorities)
+                .signWith(key, SignatureAlgorithm.HS256).compact();
+    }
+
+    /**
+     * <p>
+     *     Generate refresh Token by given now date. Sign with given key in {@code application.properties} with HS256.
+     * </p>
+     * @param date String with java Date format
+     * @return jwt string contain expire, sign key
+     */
+    private String generateRefreshToken(long date) {
+        Date tokenExpiration = new Date(date + expireDateRefreshToken);
+
+        return Jwts.builder()
+                .setExpiration(tokenExpiration)
+                .signWith(key, SignatureAlgorithm.HS256).compact();
+    }
+
+    /**
+     * <p>
+     *     Get authorities from given authentication , private method.
+     * </p>
+     * @param authentication to get authorities
+     * @return authorities in authentication
+     */
+    private String getAuthoritiesFromAuthentication(Authentication authentication) {
+        return authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining("."));
+    }
+}
