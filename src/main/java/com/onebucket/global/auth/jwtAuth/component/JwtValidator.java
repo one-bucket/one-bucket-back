0d520e7f4473df5924357d505f5104c32dd62f63
unikal1 <49333855+unikal1@users.noreply.github.com>
2024-06-25 21:27:11 +0900

    [FEAT] Add SecurityConfig and JwtAuthenticationFilter used in config.

--- src/main/java/com/onebucket/global/auth/jwtAuth/component/JwtValidator.java
+++ src/main/java/com/onebucket/global/auth/jwtAuth/component/JwtValidator.java
@@ -1,13 +1,23 @@
 package com.onebucket.global.auth.jwtAuth.component;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
 
 /**
  * packageName : <span style="color: orange;">com.onebucket.gloabal.auth.jwtAuth.component</span> <br>
@@ -27,6 +37,7 @@ import java.security.Key;
  * DATE          Author           Note
  * ----------------------------------------------------------
  * 6/21/24        isanghyeog         first create
+ * 6/25/24        sang               add getAuthentication
  *
  * </pre>
  */
@@ -35,6 +46,7 @@ import java.security.Key;
 public class JwtValidator {
 
     private final Key key;
+    private static final String AUTHORITIES_KEY = "auth";
 
     public JwtValidator(@Value("${jwt.secret}") String secretKey) {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
@@ -47,5 +59,32 @@ public class JwtValidator {
                 .build()
                 .parseClaimsJws(token);
         return true;
+    }
+
+    public Authentication getAuthentication(String accessToken) {
+        Claims claims = parseClaims(accessToken);
+        if(claims.get(AUTHORITIES_KEY) == null) {
+            throw new NullPointerException();
+        }
+
+        Collection<? extends GrantedAuthority> authorities = Arrays.stream(
+                claims.get(AUTHORITIES_KEY).toString().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+    }
+
+    private Claims parseClaims(String accessToken) {
+        try{
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(accessToken)
+                    .getBody();
+        } catch(ExpiredJwtException e) {
+            return e.getClaims();
+        }
     }
 }
