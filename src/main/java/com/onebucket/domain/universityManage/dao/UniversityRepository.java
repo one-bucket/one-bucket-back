aa73fa026fa28102abddcf971384e53e1d010204
sleepyhoon <pokjm99@naver.com>
2024-06-30 23:31:14 +0900

    [ADD] University domain and simple test

---
+++ src/main/java/com/onebucket/domain/universityManage/dao/UniversityRepository.java
@@ -0,0 +1,33 @@
+package com.onebucket.domain.universityManage.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.onebucket.domain.universityManage.domain.University;
+
+import java.util.Optional;
+
+/**
+ * <br>package name   : com.onebucket.domain.universityManage.dao
+ * <br>file name      : UniversityRepository
+ * <br>date           : 2024-06-30
+ * <pre>
+ * <span style="color: white;">[description]</span>
+ *
+ * </pre>
+ * <pre>
+ * <span style="color: white;">usage:</span>
+ * {@code
+ *
+ * } </pre>
+ * <pre>
+ * modified log :
+ * =======================================================
+ * DATE           AUTHOR               NOTE
+ * -------------------------------------------------------
+ * 2024-06-30        SeungHoon              init create
+ * </pre>
+ */
+@Repository
+public interface UniversityRepository extends JpaRepository<University, Long> {
+    Optional<University> findByName(String name);
+}
---
+++ src/main/java/com/onebucket/domain/universityManage/domain/University.java
@@ -0,0 +1,41 @@
+package com.onebucket.domain.universityManage.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * <br>package name   : com.onebucket.domain.universityManage.domain
+ * <br>file name      : University
+ * <br>date           : 2024-06-30
+ * <pre>
+ * <span style="color: white;">[description]</span>
+ *
+ * </pre>
+ * <pre>
+ * <span style="color: white;">usage:</span>
+ * {@code
+ *
+ * } </pre>
+ * <pre>
+ * modified log :
+ * =======================================================
+ * DATE           AUTHOR               NOTE
+ * -------------------------------------------------------
+ * 2024-06-30        SeungHoon              init create
+ * </pre>
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class University {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+}
