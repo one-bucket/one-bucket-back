0ec2253233bbb803ec433de1162ceecfb58ccf1d
sleepyhoon <pokjm99@naver.com>
2024-07-08 19:21:40 +0900

    [FEAT] UniversityController 작성.

--- src/main/java/com/onebucket/domain/universityManage/controller/UniversityController.java
+++ src/main/java/com/onebucket/domain/universityManage/controller/UniversityController.java
@@ -1,7 +1,6 @@
 package com.onebucket.domain.universityManage.controller;
 
-import com.onebucket.domain.universityManage.dto.ResponseUniversityDto;
-import com.onebucket.domain.universityManage.dto.RequestUniversityDto;
+import com.onebucket.domain.universityManage.dto.*;
 import com.onebucket.domain.universityManage.service.UniversityService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +15,14 @@ import java.util.List;
  * <br>date           : 2024-07-05
  * <pre>
  * <span style="color: white;">[description]</span>
- *
+ * POST - "/admin/university/create"
+ * GET - "/admin/university"
+ * GET - "/admin/university/{id}
+ * DELETE - "/admin/university/{id}
+ * ---------------------------------------
+ * 밑의 2개는 대학 정보를 추후 수정할 필요가 있는지 결정 후 사용. 현재는 구현만 하고 주석 처리 하였음.
+ * PUT - "/admin/university/{id}/email"
+ * PUT - "/admin/university/{id}/address"
  * </pre>
  * <pre>
  * <span style="color: white;">usage:</span>
@@ -33,20 +39,44 @@ import java.util.List;
  */
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/univ")
+@RequestMapping("/admin")
 public class UniversityController {
 
     private final UniversityService universityService;
 
-    @GetMapping
+    @PostMapping("/university/create")
+    public ResponseEntity<?> createUniversity(@Valid @RequestBody CreateUniversityDto createUniversityDto) {
+        Long id = universityService.createUniversity(createUniversityDto);
+        return ResponseEntity.ok("create success, University id is " + id);
+    }
+
+    @GetMapping("/university")
     public ResponseEntity<?> getAllUniversity() {
-        List<ResponseUniversityDto> universities = universityService.findAll();
+        List<ResponseUniversityDto> universities = universityService.findAllUniversity();
         return ResponseEntity.ok(universities);
     }
 
-    @PostMapping
-    public ResponseEntity<?> getUniversity(@Valid @RequestBody RequestUniversityDto readUniversityRequestDto) {
-        ResponseUniversityDto requestDto = universityService.getUniversityByName(readUniversityRequestDto.getName());
-        return ResponseEntity.ok(requestDto);
+    @GetMapping("/university/{id}")
+    public ResponseEntity<?> getUniversity(@PathVariable Long id) {
+        ResponseUniversityDto universityDto = universityService.getUniversity(id);
+        return ResponseEntity.ok(universityDto);
+    }
+
+//    @PutMapping("/university/update/{id}/email")
+//    public ResponseEntity<?> updateUniversityEmail(@PathVariable Long id, @Valid @RequestBody UpdateUniversityEmailDto updateUniversityEmailDto) {
+//        universityService.updateUniversityEmail(id,updateUniversityEmailDto);
+//        return ResponseEntity.ok("update email success");
+//    }
+
+//    @PutMapping("/university/update/{id}/address")
+//    public ResponseEntity<?> updateUniversityAddress(@PathVariable Long id, @Valid @RequestBody UpdateUniversityAddressDto updateUniversityAddressDto) {
+//        universityService.updateUniversityAddress(id,updateUniversityAddressDto);
+//        return ResponseEntity.ok("update address success");
+//    }
+
+    @DeleteMapping("/university/delete/{id}")
+    public ResponseEntity<?> deleteUniversity(@PathVariable Long id) {
+        universityService.deleteUniversity(id);
+        return ResponseEntity.ok("delete success");
     }
 }
