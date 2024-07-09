bb9734f715f273b930d313a91e0e3e5838c71962
sleepyhoon <pokjm99@naver.com>
2024-07-09 23:01:21 +0900

    [FEAT] 채팅을 위한 Config 추가.

--- src/main/java/com/onebucket/global/redis/RedisConfig.java
+++ src/main/java/com/onebucket/global/redis/RedisConfig.java
@@ -3,11 +3,13 @@ package com.onebucket.global.redis;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 /**
@@ -61,10 +63,24 @@ public class RedisConfig {
         );
     }
 
+    /**
+     * 어플리케이션에서 사용할 redisTemplate 설정
+     */
     @Bean
     public RedisTemplate<String, String> redisTemplate() {
         StringRedisTemplate redisTemplate = new StringRedisTemplate();
         redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
     }
+
+    /**
+     * redis pub/sub 메세지를 처리하는 listener 설정
+     */
+    @Bean
+    public RedisMessageListenerContainer redisMessageListener(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        return container;
+    }
+
 }
