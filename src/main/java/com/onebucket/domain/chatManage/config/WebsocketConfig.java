54573c3758a2c4c9444e2dd1a088ff4307c16a6d
sleepyhoon <pokjm99@naver.com>
2024-07-09 23:02:20 +0900

    [FEAT] 채팅을 구현하기 위한 WebsocketConfig 추가

---
+++ src/main/java/com/onebucket/domain/chatManage/config/WebsocketConfig.java
@@ -0,0 +1,45 @@
+package com.onebucket.domain.chatManage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * <br>package name   : com.onebucket.global.auth.config
+ * <br>file name      : WebsocketConfig
+ * <br>date           : 2024-07-08
+ * <pre>
+ * <span style="color: white;">[description]</span>
+ *
+ * </pre>
+ * <pre>
+ * <span style="color: white;">usage:</span>
+ * {@code
+ *
+ * } </pre>
+ * <pre>
+ * modified log :
+ * =======================================================
+ * DATE           AUTHOR               NOTE
+ * -------------------------------------------------------
+ * 2024-07-08        SeungHoon              init create
+ * </pre>
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/sub");
+        config.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp")
+                .setAllowedOriginPatterns("*");
+    }
+}
