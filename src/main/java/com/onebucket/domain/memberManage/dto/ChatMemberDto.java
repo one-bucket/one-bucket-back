392b4c2857488287c6e19b5ae0e2a1a43aee4c94
sleepyhoon <pokjm99@naver.com>
2024-07-30 18:02:31 +0900

    feat: 채팅방에 속한 유저 dto 추가

---
+++ src/main/java/com/onebucket/domain/memberManage/dto/ChatMemberDto.java
@@ -0,0 +1,38 @@
+package com.onebucket.domain.memberManage.dto;
+
+import com.onebucket.domain.memberManage.domain.Profile;
+
+import java.time.LocalDate;
+
+/**
+ * <br>package name   : com.onebucket.domain.memberManage.dto
+ * <br>file name      : ChatMemberDto
+ * <br>date           : 2024-07-30
+ * <pre>
+ * <span style="color: white;">[description]</span>
+ * 채팅방에 속한 멤버들 정보를 담는다.
+ * </pre>
+ * <pre>
+ * <span style="color: white;">usage:</span>
+ * {@code
+ *
+ * } </pre>
+ * <pre>
+ * modified log :
+ * =======================================================
+ * DATE           AUTHOR               NOTE
+ * -------------------------------------------------------
+ * 2024-07-30        SeungHoon              init create
+ * </pre>
+ */
+public record ChatMemberDto(
+        String nickname,
+        String gender,
+        int age,
+        String description,
+        LocalDate birth
+) {
+    public static ChatMemberDto of(String nickname, Profile p) {
+        return new ChatMemberDto(nickname, p.getGender(), p.getAge(), p.getDescription(), p.getBirth());
+    }
+}
