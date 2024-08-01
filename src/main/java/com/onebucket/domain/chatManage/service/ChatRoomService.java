eea55ce65f4d57ec16724a373ffdec14ff828de6
sleepyhoon <pokjm99@naver.com>
2024-08-02 02:37:12 +0900

    [REFACTOR] : 채팅방 만들기 서비스 && 멤버 추가 서비스 이름 변경

--- src/main/java/com/onebucket/domain/chatManage/service/ChatRoomService.java
+++ src/main/java/com/onebucket/domain/chatManage/service/ChatRoomService.java
@@ -1,11 +1,10 @@
 package com.onebucket.domain.chatManage.service;
 
 import com.onebucket.domain.chatManage.domain.ChatRoom;
+import com.onebucket.domain.chatManage.dto.CreateChatRoomDto;
 import org.springframework.data.redis.listener.ChannelTopic;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
 /**
  * <br>package name   : com.onebucket.domain.chatManage.service
@@ -29,10 +28,10 @@ import java.util.Set;
  * </pre>
  */
 public interface ChatRoomService {
-    ChatRoom createChatRoom(String name);
+    ChatRoom createChatRoom(CreateChatRoomDto createChatRoomDto);
     void enterChatRoom(String roomId);
     List<ChatRoom> getChatRooms();
     ChatRoom getChatRoom(String roomId);
     ChannelTopic getTopic(String roomId);
-    void addMember(String roomId,String username);
+    void addChatRoomMember(String roomId, String username);
 }
