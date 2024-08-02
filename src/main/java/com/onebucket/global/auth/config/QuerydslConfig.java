72d8311096153e390fbd5a7f8ecfdd48fb6a8186
sleepyhoon <pokjm99@naver.com>
2024-08-02 19:59:03 +0900

    [FEAT] : Querydsl config 추가

---
+++ src/main/java/com/onebucket/global/auth/config/QuerydslConfig.java
@@ -0,0 +1,40 @@
+package com.onebucket.global.auth.config;
+
+import com.querydsl.jpa.JPQLTemplates;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <br>package name   : com.onebucket.global.auth.config
+ * <br>file name      : QuerydslConfig
+ * <br>date           : 2024-08-02
+ * <pre>
+ * <span style="color: white;">[description]</span>
+ *
+ * </pre>
+ * <pre>
+ * <span style="color: white;">usage:</span>
+ * {@code
+ *
+ * } </pre>
+ * <pre>
+ * modified log :
+ * =======================================================
+ * DATE           AUTHOR               NOTE
+ * -------------------------------------------------------
+ * 2024-08-02        SeungHoon              init create
+ * </pre>
+ */
+@Configuration
+public class QuerydslConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
+    }
+}
