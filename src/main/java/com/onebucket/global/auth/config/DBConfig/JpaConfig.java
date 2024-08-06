feac17ccf296817e4a8f3d8f324d3dbf85e468e9
sleepyhoon <pokjm99@naver.com>
2024-08-06 20:03:07 +0900

    [Refactor] : JpaConfig 와 QuerydslConfig 파일 하나로 합치기

--- src/main/java/com/onebucket/global/auth/config/DBConfig/JpaConfig.java
+++ src/main/java/com/onebucket/global/auth/config/DBConfig/JpaConfig.java
@@ -1,5 +1,10 @@
 package com.onebucket.global.auth.config.DBConfig;
 
+import com.querydsl.jpa.JPQLTemplates;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -31,4 +36,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
         "com.onebucket.domain.universityManage.dao"
 })
 public class JpaConfig {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
+    }
 }
