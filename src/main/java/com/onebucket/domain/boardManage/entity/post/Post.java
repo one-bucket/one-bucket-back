0711a0f6844e2391087497f7dc42a640de0e7518
unikal1 <jack8226123@gmail.com>
2024-08-29 16:59:18 +0900

    [FIX] 테이블 구조 변경

--- src/main/java/com/onebucket/domain/boardManage/entity/post/Post.java
+++ src/main/java/com/onebucket/domain/boardManage/entity/post/Post.java
@@ -42,6 +42,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 @EntityListeners(AuditingEntityListener.class)
 public class Post {
 
