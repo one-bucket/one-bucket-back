278c2d4173b2445df7c1089a168ffa692141b248
unikal1 <jack8226123@gmail.com>
2024-09-09 20:36:59 +0900

    [TEST] board 의 리스트를 가져오는 메서드에 대한 테스트 코드 작성
    성공 / empty return 성공 / invalid univ 실패

--- src/test/java/com/onebucket/domain/boardManage/api/BoardControllerTest.java
+++ src/test/java/com/onebucket/domain/boardManage/api/BoardControllerTest.java
@@ -2,15 +2,15 @@ package com.onebucket.domain.boardManage.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.onebucket.domain.boardManage.dto.internal.board.CreateBoardDto;
-import com.onebucket.domain.boardManage.dto.internal.board.CreateBoardsDto;
-import com.onebucket.domain.boardManage.dto.request.RequestCreateBoardDto;
+import com.onebucket.domain.boardManage.dto.internal.board.BoardIdAndNameDto;
 import com.onebucket.domain.boardManage.service.BoardService;
-import com.onebucket.global.exceptionManage.customException.boardManageException.AdminManageBoardException;
-import com.onebucket.global.exceptionManage.errorCode.BoardErrorCode;
-import com.onebucket.global.exceptionManage.errorCode.ValidateErrorCode;
+import com.onebucket.domain.memberManage.service.MemberService;
+import com.onebucket.domain.universityManage.domain.University;
+import com.onebucket.global.exceptionManage.customException.universityManageException.UniversityException;
+import com.onebucket.global.exceptionManage.errorCode.UniversityErrorCode;
 import com.onebucket.global.exceptionManage.exceptionHandler.BaseExceptionHandler;
 import com.onebucket.global.exceptionManage.exceptionHandler.DataExceptionHandler;
+import com.onebucket.global.utils.SecurityUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -21,149 +21,114 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static com.onebucket.testComponent.testUtils.JsonFieldResultMatcher.hasKey;
 import static com.onebucket.testComponent.testUtils.JsonFieldResultMatcher.hasStatus;
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 /**
- * packageName : <span style="color: orange;">com.onebucket.domain.boardManage.api</span> <br>
- * name : <span style="color: orange;">BoardControllerTest</span> <br>
- * <p>
- * <span style="color: white;">[description]</span>
- * </p>
- * see Also: <br>
- *
+ * <br>package name   : com.onebucket.domain.boardManage.api
+ * <br>file name      : BoardControllerTest
+ * <br>date           : 2024-09-09
  * <pre>
- * code usage:
- * {@code
- *
- * }
- * modified log:
- * ==========================================================
- * DATE          Author           Note
- * ----------------------------------------------------------
- * 9/2/24        isanghyeog         first create
- *
+ * <span style="color: white;">[description]</span>
+ * {@link BoardController} 에 대한 테스트코드이다.
  * </pre>
  */
+
 @ExtendWith(MockitoExtension.class)
 class BoardControllerTest {
 
     @Mock
     private BoardService boardService;
+    @Mock
+    private MemberService memberService;
+    @Mock
+    private SecurityUtils securityUtils;
 
-    private MockMvc mockMvc;
+    @Mock
+    private University mockUniversity;
 
     @InjectMocks
-    private AdminBoardController adminBoardController;
+    private BoardController boardController;
 
+    private MockMvc mockMvc;
     private ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp() {
         objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
-        RequestBuilder defaultBuilder = MockMvcRequestBuilders
-                .get("/")
+        RequestBuilder defaultBuilder = get("/")
                 .accept(MediaType.APPLICATION_JSON)
                 .characterEncoding(StandardCharsets.UTF_8)
                 .contentType(MediaType.APPLICATION_JSON);
-        mockMvc = MockMvcBuilders.standaloneSetup(adminBoardController)
+        mockMvc = MockMvcBuilders.standaloneSetup(boardController)
                 .setControllerAdvice(new BaseExceptionHandler(), new DataExceptionHandler())
                 .defaultRequest(defaultBuilder)
                 .build();
     }
 
+    //+-+-+-+-+-+-+-+-]] getBoardList [[+-+-+-+-+-+-+-+-
     @Test
-    @DisplayName("createBoard - success")
-    void testCreateBoard_success() throws Exception {
-        when(boardService.createBoard(any(CreateBoardDto.class))).thenReturn(1L);
-        RequestCreateBoardDto dto = RequestCreateBoardDto.builder()
-                .boardType("board type")
-                .university("university")
-                .name("name")
-                .description("description")
-                .build();
-        mockMvc.perform(post("/board/create")
-                .content(objectMapper.writeValueAsString(dto)))
+    @DisplayName("getBoardList - success")
+    void testGetBoardList_success() throws Exception {
+
+        List<BoardIdAndNameDto> boardIdAndNameDtoList = new ArrayList<>();
+        for(long i = 1L; i <= 10L; i++) {
+            BoardIdAndNameDto boardIdAndNameDto = BoardIdAndNameDto.builder()
+                    .id(i)
+                    .name("name" + i)
+                    .build();
+
+            boardIdAndNameDtoList.add(boardIdAndNameDto);
+        }
+        when(securityUtils.getCurrentUsername()).thenReturn("test user");
+        when(memberService.usernameToUniversity("test user")).thenReturn(mockUniversity);
+        when(mockUniversity.getId()).thenReturn(1L);
+        when(boardService.getBoardList(1L)).thenReturn(boardIdAndNameDtoList);
+
+        mockMvc.perform(get("/board/list"))
                 .andExpect(status().isOk())
-                .andExpect(hasKey("message", "success create board"))
-                .andExpect(hasKey("id", "1"));
+                .andExpect(hasKey(boardIdAndNameDtoList))
+                .andExpect(jsonPath("$.length()").value(10));
     }
 
     @Test
-    @DisplayName("createBoard - fail / invalid data")
-    void testCreateBoard_fail_invalidData() throws Exception {
+    @DisplayName("getBoardList - success / but no board exist")
+    void testGetBoardList_success_noBoardReturn() throws Exception {
+        when(securityUtils.getCurrentUsername()).thenReturn("test user");
+        when(memberService.usernameToUniversity("test user")).thenReturn(mockUniversity);
+        when(mockUniversity.getId()).thenReturn(1L);
+        when(boardService.getBoardList(1L)).thenReturn(Collections.emptyList());
 
-        RequestCreateBoardDto dto = RequestCreateBoardDto.builder()
-                .boardType("board type")
-                .university("university")
-                .name("")
-                .description("description")
-                .build();
-
-        ValidateErrorCode code = ValidateErrorCode.INVALID_DATA;
-        mockMvc.perform(post("/board/create")
-                .content(objectMapper.writeValueAsString(dto)))
-                .andExpect(hasStatus(code))
-                .andExpect(hasKey(code))
-                .andExpect(content().string(containsString(
-                        "name: must not be blank"
-                )));
+        mockMvc.perform(get("/board/list"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(0));
     }
 
     @Test
-    @DisplayName("createBoard - fail / integration error")
-    void testCreateBoard_fail_sqlException() throws Exception {
-        RequestCreateBoardDto dto = RequestCreateBoardDto.builder()
-                .boardType("board type")
-                .university("university")
-                .name("name")
-                .description("description")
-                .build();
+    @DisplayName("getBoardList - fail / invalid university return")
+    void testGetBoardList_fail_invalidUniv() throws Exception {
+        UniversityErrorCode code = UniversityErrorCode.NOT_EXIST_UNIVERSITY;
+        UniversityException exception = new UniversityException(code);
+        when(securityUtils.getCurrentUsername()).thenReturn("test user");
+        when(memberService.usernameToUniversity("test user")).thenThrow(exception);
 
-        BoardErrorCode code = BoardErrorCode.DUPLICATE_BOARD;
-        AdminManageBoardException exception =
-                new AdminManageBoardException(code);
-        when(boardService.createBoard(any(CreateBoardDto.class)))
-                .thenThrow(exception);
-
-        mockMvc.perform(post("/board/create")
-                .content(objectMapper.writeValueAsString(dto)))
+        mockMvc.perform(get("/board/list"))
                 .andExpect(hasStatus(code))
                 .andExpect(hasKey(code));
+
+        verify(boardService, never()).getBoardList(anyLong());
     }
-
-    @Test
-    @DisplayName("createBoards - success")
-    void testCreateBoards_success() throws Exception {
-        List<CreateBoardsDto> results = new ArrayList<>();
-        for(int i = 0; i < 10; i++) {
-            CreateBoardsDto result = CreateBoardsDto.builder()
-                    .boardName("name" + i)
-                    .boardType("name" + i)
-                    .university("univ" + i)
-                    .id(Long.valueOf(i))
-                    .build();
-        }
-
-        when(boardService.createBoards()).thenReturn(results);
-
-        mockMvc.perform(post("/board/creates"))
-                .andExpect(status().isOk());
-
-    }
-
 }
