6d4fc802b0da33d928a1c59c309db3eb10c07d9d
unikal1 <jack8226123@gmail.com>
2024-09-11 00:11:58 +0900

    [TEST] postService 테스트 코드 작성

--- src/test/java/com/onebucket/domain/boardManage/service/PostServiceTest.java
+++ src/test/java/com/onebucket/domain/boardManage/service/PostServiceTest.java
@@ -1,15 +1,20 @@
 package com.onebucket.domain.boardManage.service;
 
 import com.onebucket.domain.boardManage.dao.BoardRepository;
+import com.onebucket.domain.boardManage.dao.CommentRepository;
 import com.onebucket.domain.boardManage.dao.PostRepository;
+import com.onebucket.domain.boardManage.dto.internal.comment.CreateCommentDto;
 import com.onebucket.domain.boardManage.dto.internal.post.CreatePostDto;
+import com.onebucket.domain.boardManage.dto.internal.post.DeletePostDto;
 import com.onebucket.domain.boardManage.entity.Board;
 import com.onebucket.domain.boardManage.entity.BoardType;
+import com.onebucket.domain.boardManage.entity.Comment;
 import com.onebucket.domain.boardManage.entity.post.Post;
 import com.onebucket.domain.memberManage.dao.MemberRepository;
 import com.onebucket.domain.memberManage.domain.Member;
 import com.onebucket.domain.universityManage.domain.University;
 import com.onebucket.global.exceptionManage.customException.boardManageException.BoardManageException;
+import com.onebucket.global.exceptionManage.customException.boardManageException.UserBoardException;
 import com.onebucket.global.exceptionManage.customException.memberManageExceptoin.AuthenticationException;
 import com.onebucket.global.exceptionManage.errorCode.AuthenticationErrorCode;
 import com.onebucket.global.exceptionManage.errorCode.BoardErrorCode;
@@ -22,10 +27,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -52,6 +58,8 @@ class PostServiceTest {
     @Mock
     private BoardRepository boardRepository;
     @Mock
+    private CommentRepository commentRepository;
+    @Mock
     private MemberRepository memberRepository;
     @Mock
     private SecurityUtils securityUtils;
@@ -63,6 +71,8 @@ class PostServiceTest {
     private University mockUniversity;
     @Mock
     private Post mockPost;
+    @Mock
+    private Comment mockComment;
     @Mock
     private Board mockBoard;
     @Mock
@@ -140,8 +150,213 @@ class PostServiceTest {
                 .isEqualTo(BoardErrorCode.UNKNOWN_BOARD);
 
         verify(postRepository, never()).save(any(Post.class));
-
     }
 
+    //-+-+-+-+-+-+]] deletePost [[-+-+-+-+-+-+
+    @Test
+    @DisplayName("deletePost - success")
+    void testDeletePost_success() {
+        Long memberId = 100L;
+        Long postId = 1L;
+        DeletePostDto dto = DeletePostDto.builder()
+                .id(postId)
+                .memberId(memberId)
+                .build();
 
+        when(postRepository.findById(postId)).thenReturn(Optional.of(mockPost));
+        when(mockPost.getAuthor()).thenReturn(mockMember);
+        when(mockMember.getId()).thenReturn(memberId);
+
+        postService.deletePost(dto);
+        verify(postRepository, times(1)).delete(any(Post.class));
+    }
+
+    @Test
+    @DisplayName("deletePost - fail / unknown post")
+    void testDeletePost_fail_unknownPost() {
+        Long memberId = 100L;
+        Long postId = 1L;
+        DeletePostDto dto = DeletePostDto.builder()
+                .id(postId)
+                .memberId(memberId)
+                .build();
+
+        when(postRepository.findById(postId)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> postService.deletePost(dto))
+                .isInstanceOf(BoardManageException.class)
+                .extracting("errorCode")
+                .isEqualTo(BoardErrorCode.UNKNOWN_POST);
+
+        verify(postRepository, never()).delete(any(Post.class));
+    }
+
+    @Test
+    @DisplayName("deletePost - fail / not author's post")
+    void testDeletePost_fail_unmatchedUser() {
+
+        Long memberId = 100L;
+        Long postId = 1L;
+        DeletePostDto dto = DeletePostDto.builder()
+                .id(postId)
+                .memberId(memberId)
+                .build();
+
+        when(postRepository.findById(postId)).thenReturn(Optional.of(mockPost));
+        when(mockPost.getAuthor()).thenReturn(mockMember);
+        when(mockMember.getId()).thenReturn(101L);
+
+        assertThatThrownBy(() -> postService.deletePost(dto))
+                .isInstanceOf(AuthenticationException.class)
+                .hasMessageContaining("you are not allowed to edit this post")
+                .extracting("errorCode")
+                .isEqualTo(AuthenticationErrorCode.UNAUTHORIZED_ACCESS);
+
+        verify(postRepository, never()).delete(any(Post.class));
+    }
+
+    @Test
+    @DisplayName("deletePost - fail / no author in post") //becuase author may exit this service.
+    void testDeletePost_fail_noAuthorInPost() {
+        Long memberId = 100L;
+        Long postId = 1L;
+        DeletePostDto dto = DeletePostDto.builder()
+                .id(postId)
+                .memberId(memberId)
+                .build();
+
+        when(postRepository.findById(postId)).thenReturn(Optional.of(mockPost));
+        when(mockPost.getAuthor()).thenReturn(null);
+
+        assertThatThrownBy(() -> postService.deletePost(dto))
+                .isInstanceOf(BoardManageException.class)
+                .extracting("errorCode")
+                .isEqualTo(BoardErrorCode.I_AM_AN_APPLE_PIE);
+    }
+
+    //-+-+-+-+-+-+]] addCommentToPost [[-+-+-+-+-+-+
+    @Test
+    @DisplayName("addCommentToPost - success / parent comment(1st layer)")
+    void testAddCommentToPost_success_1stLayer() {
+        String username = "username";
+        Long postId = 1L;
+        CreateCommentDto dto = CreateCommentDto.builder()
+                .username(username)
+                .text("text")
+                .postId(postId)
+                .build();
+
+        when(postRepository.findById(postId)).thenReturn(Optional.of(mockPost));
+        when(memberRepository.findByUsername(username)).thenReturn(Optional.of(mockMember));
+
+        postService.addCommentToPost(dto);
+        verify(mockPost, times(1)).addComment(any(Comment.class));
+        verify(postRepository, times(1)).save(mockPost);
+    }
+
+    @Test
+    @DisplayName("addCommentToPost - success / child comment(2nd layer)")
+    void testAddCommentToPost_success_2ndLayer() {
+        String username = "username";
+        Long postId = 1L;
+        Long parentCommentId = 100L;
+        CreateCommentDto dto = CreateCommentDto.builder()
+                .username(username)
+                .text("text")
+                .postId(postId)
+                .parentCommentId(parentCommentId)
+                .build();
+
+        List<Comment> replies = new ArrayList<>();
+        for(long i = 100L; i <= 110L; i++) {
+            Comment comment = Comment.builder()
+                    .id(i)
+                    .layer(0)
+                    .build();
+
+            replies.add(comment);
+        }
+
+        when(postRepository.findById(postId)).thenReturn(Optional.of(mockPost));
+        when(memberRepository.findByUsername(username)).thenReturn(Optional.of(mockMember));
+        when(mockPost.getComments()).thenReturn(replies);
+
+        postService.addCommentToPost(dto);
+
+        verify(commentRepository, times(1)).save(any(Comment.class));
+    }
+
+    @Test
+    @DisplayName("addCommentToPost - fail / 3rd layer")
+    void testAddCommentToPost_fail_3rdLayer() {
+        String username = "username";
+        Long postId = 1L;
+        Long parentCommentId = 100L;
+        CreateCommentDto dto = CreateCommentDto.builder()
+                .username(username)
+                .text("text")
+                .postId(postId)
+                .parentCommentId(parentCommentId)
+                .build();
+
+        List<Comment> replies = new ArrayList<>();
+        for(long i = 100L; i <= 110L; i++) {
+            Comment comment = Comment.builder()
+                    .id(i)
+                    .layer(1)
+                    .build();
+
+            replies.add(comment);
+        }
+
+        when(postRepository.findById(postId)).thenReturn(Optional.of(mockPost));
+        when(memberRepository.findByUsername(username)).thenReturn(Optional.of(mockMember));
+        when(mockPost.getComments()).thenReturn(replies);
+
+        assertThatThrownBy(() -> postService.addCommentToPost(dto))
+                .isInstanceOf(BoardManageException.class)
+                        .extracting("errorCode")
+                .isEqualTo(BoardErrorCode.COMMENT_LAYER_OVERHEAD);
+
+        verify(commentRepository, never()).save(any(Comment.class));
+        verify(postRepository, never()).save(any(Post.class));
+    }
+
+    @Test
+    @DisplayName("addCommentToPost - fail / unknown parent comment")
+    void testAddCommentToPost_fail_unknownParentComment() {
+        String username = "username";
+        Long postId = 1L;
+        Long parentCommentId = 100L;
+        CreateCommentDto dto = CreateCommentDto.builder()
+                .username(username)
+                .text("text")
+                .postId(postId)
+                .parentCommentId(parentCommentId)
+                .build();
+
+        List<Comment> replies = new ArrayList<>();
+        for(long i = 101L; i <= 110L; i++) {
+            Comment comment = Comment.builder()
+                    .id(i)
+                    .layer(1)
+                    .build();
+
+            replies.add(comment);
+        }
+
+        when(postRepository.findById(postId)).thenReturn(Optional.of(mockPost));
+        when(memberRepository.findByUsername(username)).thenReturn(Optional.of(mockMember));
+        when(mockPost.getComments()).thenReturn(replies);
+
+        assertThatThrownBy(() -> postService.addCommentToPost(dto))
+                .isInstanceOf(UserBoardException.class)
+                .extracting("errorCode")
+                .isEqualTo(BoardErrorCode.UNKNOWN_COMMENT);
+
+        verify(commentRepository, never()).save(any(Comment.class));
+        verify(postRepository, never()).save(any(Post.class));
+    }
+
+    //-+-+-+-+-+-+]] increaseViewCount [[-+-+-+-+-+-+
 }
