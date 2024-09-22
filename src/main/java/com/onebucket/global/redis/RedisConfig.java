db8ff0c15f725e7dc9c03b11400bbe3927a8f90c
sleepyhoon <pokjm99@naver.com>
2024-09-22 17:23:45 +0900

    [FIX] RedisTemplate 수정

--- src/main/java/com/onebucket/global/redis/RedisConfig.java
+++ src/main/java/com/onebucket/global/redis/RedisConfig.java
@@ -72,11 +72,11 @@ public class RedisConfig {
      */
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(ChatMessageDto.class));
-        return template;
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
     }
 
     @Bean
