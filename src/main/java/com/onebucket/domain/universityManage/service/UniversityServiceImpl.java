9e169808c1b9b01ae364662b670e3473106ae478
sleepyhoon <101882530+sleepyhoon@users.noreply.github.com>
2024-09-26 18:16:35 +0900

    [FEAT] redis 저장 시 유효성 검사 강화

--- src/main/java/com/onebucket/domain/universityManage/service/UniversityServiceImpl.java
+++ src/main/java/com/onebucket/domain/universityManage/service/UniversityServiceImpl.java
@@ -1,8 +1,6 @@
 package com.onebucket.domain.universityManage.service;
 
-import com.onebucket.domain.memberManage.dao.MemberRepository;
 import com.onebucket.domain.memberManage.dao.ProfileRepository;
-import com.onebucket.domain.memberManage.domain.Profile;
 import com.onebucket.domain.universityManage.dao.UniversityRepository;
 import com.onebucket.domain.universityManage.domain.University;
 import com.onebucket.domain.universityManage.dto.university.DeleteUniversityDto;
@@ -10,14 +8,16 @@ import com.onebucket.domain.universityManage.dto.university.ResponseUniversityDt
 import com.onebucket.domain.universityManage.dto.university.UpdateUniversityDto;
 import com.onebucket.domain.universityManage.dto.verifiedCode.internal.VerifiedCodeCheckDto;
 import com.onebucket.domain.universityManage.dto.verifiedCode.internal.VerifiedCodeDto;
-import com.onebucket.global.exceptionManage.customException.memberManageExceptoin.AuthenticationException;
 import com.onebucket.global.exceptionManage.customException.universityManageException.UniversityException;
-import com.onebucket.global.exceptionManage.errorCode.AuthenticationErrorCode;
+import com.onebucket.global.exceptionManage.customException.verificationException.VerificationException;
 import com.onebucket.global.exceptionManage.errorCode.UniversityErrorCode;
+import com.onebucket.global.exceptionManage.errorCode.ValidateErrorCode;
+import com.onebucket.global.exceptionManage.errorCode.VerificationErrorCode;
 import com.onebucket.global.redis.RedisRepository;
 import com.onebucket.global.utils.EntityUtils;
 import com.onebucket.global.utils.RandomStringUtils;
 import jakarta.transaction.Transactional;
+import jakarta.validation.ValidationException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -57,8 +57,8 @@ public class UniversityServiceImpl implements UniversityService {
     private final RandomStringUtils randomStringUtils;
     private final RedisRepository redisRepository;
     private final ProfileRepository profileRepository;
-    private final MemberRepository memberRepository;
 
+    private static final long EXPIRED_TIME = 5L;
     /**
      * 새로운 대학 정보를 만들고 만든 대학 정보를 반환한다. 같은 이름을 가진 대학교는 추가할 수 없음.
      * @param responseUniversityDto 생성하고자 하는 대학의 정보를 담는다.
@@ -126,6 +126,7 @@ public class UniversityServiceImpl implements UniversityService {
         University university = universityRepository.findByName(dto.getName())
                 .orElseThrow(()->new UniversityException(UniversityErrorCode.NOT_EXIST_UNIVERSITY));
 
+
         EntityUtils.updateIfNotNull(dto.getAddress(),university::setAddress);
         EntityUtils.updateIfNotNull(dto.getEmail(),university::setEmail);
     }
@@ -146,7 +147,7 @@ public class UniversityServiceImpl implements UniversityService {
         String storedCode = redisRepository.get(dto.username());
         // 인증 코드 검증
         if (storedCode == null || !storedCode.equals(dto.verifiedCode())) {
-            throw new UniversityException(UniversityErrorCode.INVALID_VERIFICATION_CODE);
+            throw new VerificationException(VerificationErrorCode.INVALID_VERIFICATION_CODE);
         }
     }
 
@@ -154,13 +155,14 @@ public class UniversityServiceImpl implements UniversityService {
     public String makeVerifiedCode(VerifiedCodeDto dto) {
         University university = universityRepository.findByName(dto.university())
                 .orElseThrow(() -> new UniversityException(UniversityErrorCode.NOT_EXIST_UNIVERSITY));
+
         // 유효한 이메일인지 확인
         validateUniversityEmail(dto, university);
+
         // 인증 코드 생성
         String verifiedCode = randomStringUtils.generateRandomStr(6);
-        long EXPIRED_TIME = 5L;
         redisRepository.save()
-                        .key(dto.username())
+                        .key(createVerificationCodeKey(dto.username(), dto.universityEmail()))
                         .value(verifiedCode)
                         .timeout(EXPIRED_TIME)
                         .timeUnit(TimeUnit.MINUTES)
@@ -169,12 +171,40 @@ public class UniversityServiceImpl implements UniversityService {
     }
 
     private void validateUniversityEmail(VerifiedCodeDto dto, University university) {
+        // 이메일 형식이 맞는지 확인
         if(!dto.universityEmail().endsWith(university.getEmail())) {
-            throw new UniversityException(UniversityErrorCode.INVALID_EMAIL);
+            throw new VerificationException(VerificationErrorCode.INVALID_EMAIL);
         }
-        // redis에 이미 존재하는 이메일인지?
-        if(redisRepository.isTokenExists(dto.username())) {
-            throw new AuthenticationException(AuthenticationErrorCode.DUPLICATE_USER);
+
+        // redis 에 관련 정보가 저장되어 있는지, 다른 이용자가 동일한 이메일로 인증하고 있는 중인지 체크
+        String emailOwnershipKey = createEmailOwnershipKey(dto.universityEmail());
+        String currentOwner = redisRepository.get(emailOwnershipKey);
+
+        if(currentOwner == null) {
+            // 현 이용자가 이메일 인증을 하고 있음을 저장한다.
+            redisRepository.save()
+                    .key(emailOwnershipKey)
+                    .value(dto.username())
+                    .timeout(EXPIRED_TIME)
+                    .timeUnit(TimeUnit.MINUTES)
+                    .save();
+        } else if (!currentOwner.equals(dto.username())) {
+            // 이미 동일한 email로 다른 사용자가 인증을 시도하고 있음.
+            throw new VerificationException(VerificationErrorCode.DUPLICATE_EMAIL);
         }
+        String username = dto.username();
+        String universityEmail = dto.universityEmail();
+
+        // 이미 db에 저장되어 있는 이메일인지 확인
+        if(profileRepository.existsByEmail(universityEmail)) {
+            throw new VerificationException(VerificationErrorCode.DUPLICATE_EMAIL);
+        }
+    }
+
+    private String createVerificationCodeKey(String username, String universityEmail) {
+        return username + ":" + universityEmail;
+    }
+    private String createEmailOwnershipKey(String universityEmail) {
+        return "emailOwnership:" + universityEmail;
     }
 }
