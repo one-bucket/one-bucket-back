e95e530251839a6a20d51ee359a039f5a40ddc03
unikal1 <jack8226123@gmail.com>
2024-10-17 21:35:13 +0900

    [FEAT] 채팅방에 멤버 추가/탈퇴 정보 조회 api 추가

--- src/main/java/com/onebucket/domain/chatManager/api/ChatRoomController.java
+++ src/main/java/com/onebucket/domain/chatManager/api/ChatRoomController.java
@@ -5,12 +5,12 @@ import com.onebucket.domain.chatManager.service.ChatRoomService;
 import com.onebucket.domain.memberManage.service.MemberService;
 import com.onebucket.global.utils.SecurityUtils;
 import com.onebucket.global.utils.SuccessResponseDto;
+import com.onebucket.global.utils.SuccessResponseWithIdDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -50,6 +50,39 @@ public class ChatRoomController {
 
         return ResponseEntity.ok(new SuccessResponseDto(id));
 
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity<SuccessResponseWithIdDto> joinRoom(@RequestParam String roomId) {
+        String username = securityUtils.getCurrentUsername();
+        Long userId = memberService.usernameToId(username);
+        ChatRoomDto.ManageMember dto = ChatRoomDto.ManageMember.builder()
+                .roomId(roomId)
+                .memberId(userId)
+                .build();
+
+        Long count = chatRoomService.addMember(dto);
+
+        return ResponseEntity.ok(new SuccessResponseWithIdDto("success add member", count));
+    }
+
+    @DeleteMapping("/quit")
+    public ResponseEntity<SuccessResponseWithIdDto> quitRoom(@RequestParam String roomId) {
+        String username = securityUtils.getCurrentUsername();
+        Long userId = memberService.usernameToId(username);
+        ChatRoomDto.ManageMember dto = ChatRoomDto.ManageMember.builder()
+                .roomId(roomId)
+                .memberId(userId)
+                .build();
+
+        Long count = chatRoomService.quitMember(dto);
+
+        return ResponseEntity.ok(new SuccessResponseWithIdDto("success quit member", count));
+    }
+
+    @GetMapping("/memberList")
+    public ResponseEntity<List<ChatRoomDto.MemberInfo>> getRoomMember(@RequestParam String roomId) {
+        return ResponseEntity.ok(chatRoomService.getMemberList(roomId));
     }
 
 }
