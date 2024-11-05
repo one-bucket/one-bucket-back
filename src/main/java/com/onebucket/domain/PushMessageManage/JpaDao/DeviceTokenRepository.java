40ce1a7b5d5736d2613c365a61a6f35faf873380
unikal1 <49333855+unikal1@users.noreply.github.com>
2024-11-05 21:08:15 +0900

    [FEAT] 디바이스 토큰 관리 로직

---
+++ src/main/java/com/onebucket/domain/PushMessageManage/JpaDao/DeviceTokenRepository.java
@@ -0,0 +1,27 @@
+package com.onebucket.domain.PushMessageManage.JpaDao;
+
+import com.onebucket.domain.PushMessageManage.Entity.DeviceToken;
+import com.onebucket.domain.memberManage.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * <br>package name   : com.onebucket.domain.PushMessageManage.dao
+ * <br>file name      : DeviceTokenRepository
+ * <br>date           : 11/5/24
+ * <pre>
+ * <span style="color: white;">[description]</span>
+ *
+ * </pre>
+ * <pre>
+ * <span style="color: white;">usage:</span>
+ * {@code
+ *
+ * } </pre>
+ */
+@Repository
+public interface DeviceTokenRepository extends JpaRepository<DeviceToken, String> {
+    Optional<DeviceToken> findByMember(Member member);
+}
--- src/main/java/com/onebucket/domain/PushMessageManage/service/FcmDataManageServiceImpl.java
+++ src/main/java/com/onebucket/domain/PushMessageManage/service/FcmDataManageServiceImpl.java
@@ -1,14 +1,21 @@
 package com.onebucket.domain.PushMessageManage.service;
 
+import com.onebucket.domain.PushMessageManage.Entity.DeviceToken;
+import com.onebucket.domain.PushMessageManage.Entity.PushMessageLog.PushMessageLog;
+import com.onebucket.domain.PushMessageManage.JpaDao.DeviceTokenRepository;
+import com.onebucket.domain.PushMessageManage.MongoDao.PushMessageLogRepository;
+import com.onebucket.domain.PushMessageManage.dto.MessageLogDto;
 import com.onebucket.domain.PushMessageManage.dto.TokenDto;
 import com.onebucket.domain.memberManage.dao.MemberRepository;
 import com.onebucket.domain.memberManage.domain.Member;
 import com.onebucket.global.exceptionManage.customException.memberManageExceptoin.AuthenticationException;
 import com.onebucket.global.exceptionManage.errorCode.AuthenticationErrorCode;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 /**
  * <br>package name   : com.onebucket.domain.PushMessageManage.service
@@ -30,16 +37,45 @@ import org.springframework.transaction.annotation.Transactional;
 public class FcmDataManageServiceImpl {
 
     private final MemberRepository memberRepository;
+    private final DeviceTokenRepository deviceTokenRepository;
+    private final PushMessageLogRepository pushMessageLogRepository;
 
     @Transactional
-    public void saveDeviceToken(TokenDto.Info dto) {
+    public void updateDeviceToken(TokenDto.Info dto) {
+        String token = dto.getToken();
         Member member = findMember(dto.getUserId());
 
-        member.setDeviceToken(dto.getToken());
-        memberRepository.save(member);
+        Optional<DeviceToken> deviceToken = deviceTokenRepository.findByMember(member);
+        if(deviceToken.isEmpty()) {
+            DeviceToken newToken = DeviceToken.builder()
+                    .deviceToken(token)
+                    .member(member)
+                    .build();
+
+            newToken.dateUpdate();
+
+            deviceTokenRepository.save(newToken);
+        } else {
+            DeviceToken findToken = deviceToken.get();
+            findToken.dateUpdate();
+
+            if(!findToken.getDeviceToken().equals(dto.getToken())) {
+                findToken.setDeviceToken(token);
+            }
+
+            deviceTokenRepository.save(findToken);
+        }
     }
 
-    public void saveMessageLog
+    public void saveMessageLog(MessageLogDto.Info dto) {
+        LocalDateTime now = LocalDateTime.now();
+        PushMessageLog pushMessageLog = PushMessageLog.builder()
+                .title(dto.getTitle())
+                .body(dto.getBody())
+                .image(dto.getImage())
+                .sendAt(now)
+                .build();
+    }
 
     private Member findMember(Long userId) {
         return memberRepository.findById(userId).orElseThrow(() ->
