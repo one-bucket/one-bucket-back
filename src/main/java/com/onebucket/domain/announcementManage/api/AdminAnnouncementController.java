839ebeae719d0d1dc984d35822226935f6adf6d3
sleepyhoon <pokjm99@gmail.com>
2024-11-08 17:36:15 +0900

    [FEAT] 중요한 공지사항의 경우 푸시 알림 가능

--- src/main/java/com/onebucket/domain/announcementManage/api/AdminAnnouncementController.java
+++ src/main/java/com/onebucket/domain/announcementManage/api/AdminAnnouncementController.java
@@ -1,6 +1,7 @@
 package com.onebucket.domain.announcementManage.api;
 
 import com.onebucket.domain.announcementManage.dto.AnnouncementDto;
+import com.onebucket.domain.announcementManage.dto.AnnouncementDto.RequestCreate;
 import com.onebucket.domain.announcementManage.service.AnnouncementService;
 import com.onebucket.global.utils.SuccessResponseDto;
 import com.onebucket.global.utils.SuccessResponseWithIdDto;
@@ -27,11 +28,18 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin/notice")
 @RequiredArgsConstructor
 public class AdminAnnouncementController {
+
     private final AnnouncementService announcementService;
 
     @PostMapping("/create")
     public ResponseEntity<SuccessResponseWithIdDto> create(@RequestBody AnnouncementDto.RequestCreate dto) {
-        Long id = announcementService.createAnnouncement((AnnouncementDto.Create) dto);
+        AnnouncementDto.Create announcementCreateDto = AnnouncementDto.Create.builder()
+                                                .text(dto.getText())
+                                                .title(dto.getTitle())
+                                                .build();
+        Long id = announcementService.createAnnouncement(announcementCreateDto);
+
+        sendPushNotificationIfNeeded(dto);
 
         return ResponseEntity.ok(new SuccessResponseWithIdDto("success create announce", id));
     }
@@ -40,5 +48,12 @@ public class AdminAnnouncementController {
     public ResponseEntity<SuccessResponseDto> delete(@PathVariable Long id) {
         announcementService.deleteAnnouncement(id);
         return ResponseEntity.ok(new SuccessResponseDto("success delete announcement"));
+    }
+
+
+    private void sendPushNotificationIfNeeded(RequestCreate dto) {
+        if(Boolean.TRUE.equals(dto.getSendPushMessage())) {
+            // 푸시 알림 메세지 전송
+        }
     }
 }
